063003b8b67ba8d813fcf434d68d2008e482d5c4
Dominique de Boer <dominiquedeboer@yahoo.nl>
2025-10-30 15:31:11 +0100

    legend

--- src/temp_given_day.cxx
+++ src/temp_given_day.cxx
@@ -1,164 +1,164 @@
 #include "../include/temp_given_day.h"
-
-#include <fstream>  //for reading the file
 #include <iostream>
+#include <fstream> //for reading the file
 #include <numeric>
 
-// for ROOT
+//for ROOT 
 #include "TCanvas.h"
-#include "TF1.h"
 #include "TH1D.h"
+#include "TF1.h"
 #include "TLatex.h"
 #include "TStyle.h"
+#include "TLegend.h"
+
 
 // default constructor - ROOT needs the default one where everything is set to 0
 temp_given_day::temp_given_day() {}
-// constructor
-temp_given_day::temp_given_day(const std::string &filename)
-    : inputFilename(filename) {}  // this one is used in main
+//constructor
+temp_given_day::temp_given_day(const std::string &filename) : inputFilename(filename) {} //this one is used in main
+
 
 void temp_given_day::ReadFile() {
-  std::ifstream file(inputFilename);  //! needs to be changed if position of
-                                      //! lund_cleaned.txt is moved
-  std::string date, time;             // initialize variables
-  double degrees;
+    std::ifstream file(inputFilename); //! needs to be changed if position of lund_cleaned.txt is moved 
+    std::string date, time; //initialize variables
+    double degrees;
 
-  std::string header;          // gets first line
-  std::getline(file, header);  // skips first line
+    std::string header; //gets first line 
+    std::getline(file, header); //skips first line
 
-  while (file >> date >> time >> degrees) {    // iterates through file
-    int year = std::stoi(date.substr(0, 4));   // extracts the year from date
-    int month = std::stoi(date.substr(5, 2));  // extracts the month from date
-    int day = std::stoi(date.substr(8, 2));    // extracts the day from date
-
-    if ((month == 2) && (day == 15)) {
-      if (year >= 1863 && year <= 1913) {
-        years_1.push_back(year);
-        temperatures_1.push_back(degrees);
-      } else if (year >= 1972 && year <= 2022) {
-        years_2.push_back(year);
-        temperatures_2.push_back(degrees);
-      }
+    while (file >> date >> time >> degrees) { //iterates through file 
+        int year = std::stoi(date.substr(0, 4)); //extracts the year from date 
+        int month = std::stoi(date.substr(5, 2)); //extracts the month from date 
+        int day = std::stoi(date.substr(8, 2)); //extracts the day from date 
+        
+        if ((month == 2) && (day == 15)) {
+            if (year >= 1863 && year <= 1913) {
+                years_1.push_back(year);
+                temperatures_1.push_back(degrees);
+            } else if (year >= 1972 && year <= 2022) {
+                years_2.push_back(year);
+                temperatures_2.push_back(degrees);
+            }
+        }
     }
-  }
-
-  file.close();
+    file.close();
 }
 
 void temp_given_day::GetAverageTemp_1() {
-  unique_years_1.clear();  // so that nothing from earlier is included
-  avg_temp_1.clear();
+    unique_years_1.clear(); //so that nothing from earlier is included 
+    avg_temp_1.clear();
+    
+    double sum = 0.0; //initialize sum and count
+    int count = 0;
 
-  double sum = 0.0;  // initialize sum and count
-  int count = 0;
+    int first_year = 1863;
 
-  int first_year = 1863;
+    for (size_t i=0; i < years_1.size(); i++) {
+        if (years_1[i] == first_year) { //sums all temperatures from one year
+            sum += temperatures_1[i];
+            count++; //count the amount of entries from that year
+        } 
+        else {
+            avg_temp_1.push_back(sum/count); //adds mean to avg_years_1
+            unique_years_1.push_back(first_year);
 
-  for (size_t i = 0; i < years_1.size(); i++) {
-    if (years_1[i] == first_year) {  // sums all temperatures from one year
-      sum += temperatures_1[i];
-      count++;  // count the amount of entries from that year
-    } else {
-      avg_temp_1.push_back(sum / count);  // adds mean to avg_years_1
-      unique_years_1.push_back(first_year);
-
-      first_year = years_1[i];  // goes to next year
-      count =
-          1;  // count set back to 1 (not 0 because we are already in the loop)
-      sum = temperatures_1[i];  // adds first temperature in new year to sum
+            first_year = years_1[i]; //goes to next year
+            count = 1; //count set back to 1 (not 0 because we are already in the loop)
+            sum = temperatures_1[i]; //adds first temperature in new year to sum
+        }
     }
-  }
-  // last year needs to be added manually
-  avg_temp_1.push_back(sum / count);
-  unique_years_1.push_back(first_year);
+    //last year needs to be added manually
+    avg_temp_1.push_back(sum/count);
+    unique_years_1.push_back(first_year);
 }
 
 void temp_given_day::GetAverageTemp_2() {
-  unique_years_2.clear();  // so that nothing from earlier is included
-  avg_temp_2.clear();
+    
+    unique_years_2.clear(); //so that nothing from earlier is included 
+    avg_temp_2.clear();
+    
+    double sum = 0.0; //initialize sum and count
+    int count = 0;
 
-  double sum = 0.0;  // initialize sum and count
-  int count = 0;
+    int first_year = 1972;
 
-  int first_year = 1972;
+    for (size_t i=0; i < years_2.size(); i++) {
+        if (years_2[i] == first_year) { //sums all temperatures from one year
+            sum += temperatures_2[i];
+            count++; //count the amount of entries from that year
+        } 
+        else {
+            avg_temp_2.push_back(sum/count); //adds mean to avg_years_1
+            unique_years_2.push_back(first_year);
 
-  for (size_t i = 0; i < years_2.size(); i++) {
-    if (years_2[i] == first_year) {  // sums all temperatures from one year
-      sum += temperatures_2[i];
-      count++;  // count the amount of entries from that year
-    } else {
-      avg_temp_2.push_back(sum / count);  // adds mean to avg_years_1
-      unique_years_2.push_back(first_year);
-
-      first_year = years_2[i];  // goes to next year
-      count =
-          1;  // count set back to 1 (not 0 because we are already in the loop)
-      sum = temperatures_2[i];  // adds first temperature in new year to sum
+            first_year = years_2[i]; //goes to next year
+            count = 1; //count set back to 1 (not 0 because we are already in the loop)
+            sum = temperatures_2[i]; //adds first temperature in new year to sum
+        }
     }
-  }
-  // last year needs to be added manually
-  avg_temp_2.push_back(sum / count);
-  unique_years_2.push_back(first_year);
+    //last year needs to be added manually
+    avg_temp_2.push_back(sum/count);
+    unique_years_2.push_back(first_year);
 }
 
 void temp_given_day::CreateHistogram() {
-  std::cout << "Period 1: " << avg_temp_1.size() << " entries" << std::endl;
-  std::cout << "Period 2: " << avg_temp_2.size() << " entries" << std::endl;
-  // first period
-  auto *histogram_1 = new TH1D(
-      "histogram1",
-      "Temperature on 15/02 from 1863-1913 ;Temperature [#circC];Entries", 50,
-      -15, 15);
-  histogram_1->SetFillColor(kBlue + 1);  // colour of bars is blue
-  histogram_1->SetLineColor(kBlack);     // outline black
-  for (size_t i = 0; i < unique_years_1.size(); ++i) {
-    histogram_1->Fill(avg_temp_1[i]);
-  }
+    std::cout << "Period 1: " << avg_temp_1.size() << " entries" << std::endl;
+    std::cout << "Period 2: " << avg_temp_2.size() << " entries" << std::endl;
+    // first period
+    auto *histogram_1 = new TH1D("histogram1", "Temperature on 15/02 from 1863-1913 ;Temperature [#circC];Entries", 50, -15, 15);
+    histogram_1->SetFillColor(kBlue +1); //colour of bars is blue
+     histogram_1->SetLineColor(kBlack); //outline black
+    for (size_t i =0 ; i< unique_years_1.size(); ++i) {
+        histogram_1->Fill(avg_temp_1[i]);
+    }
 
-  auto canvas1 = new TCanvas("canvas1", "", 800, 600);
-  histogram_1->Draw();
+    auto canvas1 = new TCanvas("canvas1","",800,600);
+    histogram_1->Draw();
 
-  TLatex text;    // for printing on canvas
-  text.SetNDC();  // normalized coordinates (0-1) starts in lower left corner
-  text.SetTextSize(0.05);
+    TLatex text; //for printing on canvas
+    text.SetNDC(); //normalized coordinates (0-1) starts in lower left corner
+    text.SetTextSize(0.05);
 
-  canvas1->SaveAs("temperature_given_day_1.pdf");
+    canvas1->SaveAs("temperature_given_day_1.pdf");
 
-  // repeat for second period
-  auto *histogram_2 = new TH1D(
-      "histogram2",
-      "Temperature on 15/02 from 1972-2022 ;Temperature [#circC];Entries", 50,
-      -15, 15);
-  histogram_2->SetFillColor(kBlue + 1);  // colour of bars is blue
-  histogram_2->SetLineColor(kBlack);     // outline black
-  for (size_t j = 0; j < unique_years_2.size(); ++j) {
-    histogram_2->Fill(avg_temp_2[j]);
-  }
+    // repeat for second period
+    auto *histogram_2 = new TH1D("histogram2", "Temperature on 15/02 from 1972-2022 ;Temperature [#circC];Entries", 50, -15, 15);
+    histogram_2->SetFillColor(kBlue +1); //colour of bars is blue
+     histogram_2->SetLineColor(kBlack); //outline black
+    for (size_t j =0 ; j< unique_years_2.size(); ++j) {
+        histogram_2->Fill(avg_temp_2[j]);
+    }
 
-  auto canvas2 = new TCanvas("canvas2", "", 800, 600);
-  histogram_2->Draw();
+    auto canvas2 = new TCanvas("canvas2","",800,600);
+    histogram_2->Draw();
 
-  TLatex text2;    // for printing on canvas
-  text2.SetNDC();  // normalized coordinates (0-1) starts in lower left corner
-  text2.SetTextSize(0.05);
+    TLatex text2; //for printing on canvas
+    text2.SetNDC(); //normalized coordinates (0-1) starts in lower left corner
+    text2.SetTextSize(0.05);
 
-  canvas2->SaveAs("temperature_given_day_2.pdf");
+    canvas2->SaveAs("temperature_given_day_2.pdf");
 
-  // combined histogram
+    // combined histogram
+    auto canvas3 = new TCanvas("canvas3","", 800, 600);
 
-  gStyle->SetOptStat(0);  // prevent new stats boxes
-  histogram_1->SetTitle(
-      "Temperature on 15/02 in 1863-1913 (blue) & 1972-2022 (red)");
-  histogram_2->SetTitle("");  // getting rid of the previous titles
-  auto canvas3 = new TCanvas("combined canvas", "", 800, 600);
-  histogram_1->Draw();
-  histogram_2->Draw("SAME");
+    histogram_1->Draw();
+    histogram_2->Draw("SAME");
+    // gStyle->SetOptStat(0); // prevent new stats boxes
+    // histogram_1->SetTitle("Temperature on 15/02 in 1863-1913 (blue) & 1972-2022 (red)"); 
+    // histogram_2->SetTitle(""); //getting rid of the previous titles
+    
 
-  canvas3->Update();  // force ROOT to draw everything
+    // canvas3->Update(); // force ROOT to draw everything
 
-  TLatex text3;    // for printing on canvas
-  text3.SetNDC();  // normalized coordinates (0-1) starts in lower left corner
-  text3.SetTextSize(0.05);
+    auto legend = new TLegend(0.65, 0.75, 0.9, 0.9);
+    legend->AddEntry(histogram_1, "1863-1913", "f");
+    legend->AddEntry(histogram_2, "1972-2022", "f");
+    legend->Draw();
 
-  canvas3->SaveAs("combined_temperature_given_day.pdf");
+    TLatex text3; //for printing on canvas
+    text3.SetNDC(); //normalized coordinates (0-1) starts in lower left corner
+    text3.SetTextSize(0.05);
+    text3.DrawLatex(0.15, 0.92, "Temperature on 15th of February");
+
+    canvas3->SaveAs("combined_temperature_given_day.pdf");
 }
